8f7b4bd4e60222a5358a9c9068bce50c3aa7574a
chengjunjiecn <34801501+chengjunjiecn@users.noreply.github.com>
2025-08-28 16:31:41 +0800

    Update awq.py

--- tests/awq.py
+++ tests/awq.py
@@ -1,21 +1,24 @@
 !pip install autoawq
 !pip install transformers
+ 
 from awq import AutoAWQForCausalLM
 from transformers import AutoTokenizer
 
-# 模型路径
-model_path = "/content/drive/MyDrive/data/model-f16.gguf"
-out_dir = "./quant_awq_gguf"
+# 模型路径（纯文本模型）
+model_path = "./qwen"  # 替换为你的模型路径
+quant_path = "./quant_awq_gguf"  # 量化后模型保存路径
 
-# 加载 HF 模型或 GGUF FP16
-model = AutoAWQForCausalLM.from_pretrained(model_path, torch_dtype="auto")
+# 加载模型和分词器
+model = AutoAWQForCausalLM.from_pretrained(model_path)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
+quant_config = {"zero_point": True, "q_group_size": 128, "w_bit": 4, "version":"GEMM"}
 
-# 执行 AWQ 量化
-quantized_model = model.quantize(bit=4, use_awq=True)  # 4-bit AWQ
+# Load model
+model = AutoAWQForCausalLM.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 
-# 保存为 GGUF
-quantized_model.save_pretrained(out_dir, save_format="gguf")
+# Quantize
+model.quantize(tokenizer, quant_config=quant_config)
 
 
 # url = "https://drive.google.com/uc?id=14zbm4468eNwWUWcYfmT27ZuoYNF3pQYM&export=download"
